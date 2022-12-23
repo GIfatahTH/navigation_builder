234163cdad45a712ea92b05f87d985565de472f8
MELLATI Fatah <meltft@gmail.com>
2022-12-23 15:53:05 +0100

    skip test if not macos

--- example/test/ex12_page_transition2_test.dart
+++ example/test/ex12_page_transition2_test.dart
@@ -8,24 +8,28 @@ void main() {
   //works independent of device default animation
   NavigationBuilder.transitionsBuilder = NavigationBuilder.transitions
       .bottomToUp(duration: const Duration(milliseconds: 300));
-  testWidgets('Test navigation logic', (tester) async {
-    await tester.pumpWidget(const MyApp());
-    await tester.tap(find.byIcon(Icons.keyboard_arrow_right));
-    await tester.pump();
-    await tester.pump();
-    await expectLater(
-        find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_1.png'));
-    await tester.pump(const Duration(milliseconds: 500));
-    await expectLater(
-        find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_2.png'));
-    await tester.pump(const Duration(milliseconds: 500));
-    await expectLater(
-        find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_3.png'));
-    navigator.back();
-    await tester.pump();
-    await tester.pump(const Duration(milliseconds: 200));
-    await expectLater(
-        find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_4.png'));
-    await tester.pumpAndSettle();
-  });
+  testWidgets(
+    'Test navigation logic',
+    (tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.tap(find.byIcon(Icons.keyboard_arrow_right));
+      await tester.pump();
+      await tester.pump();
+      await expectLater(
+          find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_1.png'));
+      await tester.pump(const Duration(milliseconds: 500));
+      await expectLater(
+          find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_2.png'));
+      await tester.pump(const Duration(milliseconds: 500));
+      await expectLater(
+          find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_3.png'));
+      navigator.back();
+      await tester.pump();
+      await tester.pump(const Duration(milliseconds: 200));
+      await expectLater(
+          find.byType(MyApp), matchesGoldenFile('./golden_files/ex11_4.png'));
+      await tester.pumpAndSettle();
+    },
+    skip: !Platform.isMacOS,
+  );
 }
